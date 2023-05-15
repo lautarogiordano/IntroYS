0ef10da0624541d7c2f32df786ce3efa2594c323
lautarogiordano <lautaro.giordano@ib.edu.ar>
2023-05-15 18:00:35 -0300

    added first parallelization

--- ys_testing/5_cluster/1_testclust.py
+++ ys_testing/5_cluster/1_testclust.py
@@ -1,209 +1,137 @@
-# %%
-import os
-
-# import matplotlib.pyplot as plt
-import networkx as nx
+import numba
+from numba import cuda, float32
+from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_uniform_float32, xoroshiro128p_uniform_float64, init_xoroshiro128p_states
+from numba.core.errors import NumbaPerformanceWarning, NumbaDeprecationWarning
+import warnings
+import cupy as cp
 import numpy as np
-import time
 from matplotlib import pyplot as plt
+import time
 
-# %%
-class model:
-    def __init__(
-        self,
-        n_agents,
-        w_min=1e-17,
-        w_0=None,
-        G=None,
-        additive=False,
-        theta=0,
-        save_every=np.inf,
-        upd_w_every=np.inf,
-        upd_graph_every=np.inf,
-        plot_every=np.inf,
-        figpath=None,
-    ):  # sourcery skip: assign-if-exp
-        self.N = n_agents
-        self.w_min = w_min
-        self.G = G
-        self.additive = additive
-        if self.G is not None:
-            self.posi = {i: self.G.nodes[i]["pos"] for i in range(self.N)}
-            self.theta = theta
-        self.update_w = upd_w_every
-        self.update_links = upd_graph_every
-        self.every = save_every
-        self.plot = plot_every
-        if self.plot != np.inf:
-            self.fig, self.ax = plt.subplots(dpi=150)
-            self.ax.set_xlim([-0.03, 1.03])
-            self.ax.set_ylim([-0.03, 1.03])
-            self.figpath = figpath
-            self.temppath = os.path.join(self.figpath, "temp")
-            self.figpath = figpath
-        # Initialize n agents with random risks and wealth between (0, 1]
-        # and normalize wealth
-        # n[i, 0] is the wealth and n[i, 1] is the risk of agent i
-        self.n = np.random.rand(self.N, 2)
-        if w_0 is not None:
-            self.n[:, 0] = w_0
-        else:
-            self.n[:, 0] = self.n[:, 0] / (np.sum(self.n[:, 0]))
-        self.gini = [self.get_gini()]
-        self.n_active = [self.get_actives()]
+warnings.simplefilter("ignore", category=NumbaPerformanceWarning)
+warnings.simplefilter("ignore", category=NumbaDeprecationWarning)
 
-    def get_opponents(self):
-        if self.G is None:
-            random_array = np.random.randint(0, self.N, self.N)
-            indices = np.arange(0, self.N)
-            # Create array of random numbers that are not equal to the index
-            # If i=j then assign j'=i+1 (j'=0 if i=N-1)
-            random_array = np.where(
-                random_array == indices, (random_array + 1) % self.N, random_array
-            )
-        else:
-            random_array = np.full(self.N, fill_value=-1)
-            for i in range(self.N):
-                if neighbors := list(nx.all_neighbors(self.G, i)):
-                    random_array[i] = np.random.choice(neighbors)
 
-        return random_array
 
-    def is_valid(self, i, j):
-        # Check if both agents have w > w_min
-        return (self.n[i, 0] > self.w_min) and (self.n[j, 0] > self.w_min)
+@cuda.jit(device=True)
+def double_lock(mutex, i, j):
+    # first = i;
+    # second = j;
+    # if i > j:
+    #     first = j;
+    #     second = i;
 
-    def get_dw(self, i, j):
-        return np.minimum(self.n[i, 0] * self.n[i, 1], self.n[j, 0] * self.n[j, 1])
+    first, second = (i, j) if i < j else (j, i)
+    
+    while cuda.atomic.cas(mutex, first, 0, 1) != 0:
+        pass
+    while cuda.atomic.cas(mutex, second, 0, 1) != 0:
+        pass
+    
+    # cuda.threadfence()
 
-    def get_gini(self):
-        w = np.sort(self.n[:, 0])
-        p_cumsum = np.cumsum(w) / np.sum(w)
-        B = np.sum(p_cumsum) / self.N
-        return 1 + 1 / self.N - 2 * B
 
-    def get_actives(self):
-        return np.sum(self.n[:, 0] > self.w_min)
+@cuda.jit(device=True)
+def double_unlock(mutex, i, j):
+    # cuda.threadfence()
+    cuda.atomic.exch(mutex, j, 0)
+    cuda.atomic.exch(mutex, i, 0)
 
-    def get_liquidity():
+def get_gini(w, N):
+    a = cp.sort(w)
+    p_cumsum = cp.cumsum(a) / cp.sum(a)
+    B = cp.sum(p_cumsum) / N
+    return 1 + 1 / N - 2 * B
+
+
+@cuda.jit
+def stream_exchange(w, r, mutex, wmin, mcs, rng_state):
+    tid = cuda.threadIdx.x
+
+    if tid >= n_agents:
         return
-
-    def update_wealth(self, i, j, dw):
-        self.n[i, 0] += dw
-        self.n[j, 0] -= dw
-
-    def choose_winner(self, i, j):
-        raise NotImplementedError("You need to choose a valid model.")
-
-    def update_weights(self):
-        w = dict(enumerate(self.n[:, 0]))
-        nx.set_node_attributes(self.G, w, "weight")
-
-    def plot_snapshot(self, mcs):
-        ## Esto se tiene que borrar y escribir mejor
-        w = dict(enumerate(self.n[:, 0]))
-        a = np.array(list(w.values()))
-
-        dead_nodes = [node for node, weight in w.items() if weight < self.w_min]
-
-        node_size = 500 * np.sqrt(a)
-        node_colors = plt.cm.coolwarm(100 * a)
-        edge_colors = [
-            "r" if (e[0] in dead_nodes or e[1] in dead_nodes) else "black"
-            for e in self.G.edges
-        ]
-
-        filename = os.path.join(self.temppath, "test_{:05d}.png".format(mcs))
-
-        self.ax.clear()
-        self.ax.set_title(f"t = {mcs}")
-        nx.draw(
-            self.G,
-            node_size=node_size,
-            width=0.2,
-            pos=self.posi,
-            node_color=node_colors,
-            edge_color=edge_colors,
-            ax=self.ax,
-        )
-        self.fig.savefig(filename, format="PNG")
-
-    def MCS(self, steps):
-        """
-        Main MC loop
-        """
-        for mcs in range(steps):
-            if mcs % self.plot == 0 and self.G is not None:
-                self.plot_snapshot(mcs)
-
-            j = self.get_opponents()
-
-            for i, ji in enumerate(j):
-                # Check both agents have w > w_min and node is not isolated
-                if self.is_valid(i, ji) and ji != -1:
-                    dw = self.get_dw(i, ji)
-                    winner = self.choose_winner(i, ji)
-                    dw = np.where(winner == i, dw, -dw)
-                    self.update_wealth(i, ji, dw)
-
-            # After self.update_w update weights
-            if mcs % self.update_w == 0 and self.G is not None:
-                self.update_weights()
-
-            # Recompute the links if the network is dynamic
-            if (mcs + 1) % self.update_links == 0 and self.G is not None:
-                self.G = nx.geographical_threshold_graph(
-                    self.N,
-                    theta=self.theta,
-                    weight=self.n[:, 0],
-                    dim=2,
-                    pos=self.posi,
-                    additive=self.additive,
-                )
-            # After self.every MCS append new Gini index
-            if (mcs + 1) % self.every == 0:
-                self.gini.append(self.get_gini())
-                self.n_active.append(self.get_actives())
+    
+    for t in range(mcs):
+        opp = int(xoroshiro128p_uniform_float32(rng_state, tid) * (n_agents))
 
 
-# %%
-class YSmodel(model):
-    def __init__(self, n_agents=200, f=0, **kwargs):
-        super().__init__(n_agents, **kwargs)
-        # f is the social protection factor
-        self.f = f
+        # Check if both agents have enough wealth to exchange
+        if opp != tid and w[tid] > wmin and w[opp] > wmin:
+            double_lock(mutex, tid, opp)
 
-    def choose_winner(self, i, j):
-        p = 0.5 + self.f * (
-            (self.n[j, 0] - self.n[i, 0]) / (self.n[i, 0] + self.n[j, 0])
-        )
-        return np.random.choice([i, j], p=[p, 1 - p])
+            dw = min(r[tid] * w[tid], r[opp] * w[opp]) 
 
-# Numero de nodos = Numero de agentes
-n = 1000
-# Pesos de los nodos = riquezas iniciales de los agentes
-a_0 = np.random.rand(n)
-a_0 /= np.sum(a_0)
-w_0 = dict(enumerate(a_0))
+            # Compute the probability of winning
+            prob_win = xoroshiro128p_uniform_float32(rng_state, tid)
+            p = 0.5 + 0.4 * (w[opp] - w[tid]) / (w[tid] + w[opp])
+            # Determine the winner and loser
+            dw = dw if prob_win <= p else -dw
+            
+            w[tid] += dw
+            w[opp] -= dw
 
-f_set = [0]
+            double_unlock(mutex, tid, opp)
+    
+        cuda.syncthreads()
 
-MCS = 1000
+n_streams = 1000
 
-upd_w = np.inf
-upd_g = np.inf
-plot_every = upd_w
+n_agents = 1000
+wmin = 1e-17
+mcs = 1000
 
+with cuda.defer_cleanup():
+    # Create 10 streams
+    streams = [cuda.stream() for _ in range(n_streams)]
 
-# %%
-for f in f_set:
-    model = YSmodel(f=f, n_agents=n, w_min=1e-17, save_every=1)
+    # Create base arrays
+    wealths = [
+        np.random.rand(n_agents).astype(np.float32) for i in range(n_streams)
+    ]
+    risks = [
+        np.random.rand(n_agents).astype(np.float32) for i in range(n_streams)
+    ]
+    mutexes = [
+        np.zeros((n_agents,), dtype=np.int32) for i in range(n_streams)
+    ]
 
-    start = time.time()
-    model.MCS(MCS)
-    end = time.time()
-    net_time = end - start
-    print("Elapsed time: {:.2f} s.".format(net_time))
+    rng_states = [
+        create_xoroshiro128p_states(n_agents, seed=time.time()) for i in range(n_streams)
+    ]
 
+    for i, wi in enumerate(wealths):
+        wi /= np.sum(wi)
 
-# %%
+    events_beg = []
+    events_end = []
+    for i, (stream, wi, ri, mutex, rng_state) in enumerate(zip(streams, wealths, risks, mutexes, rng_states)):
+        with cuda.pinned(wi, ri, mutex):
+            event_beg = cuda.event()
+            event_end = cuda.event()
+            event_beg.record(stream=stream)
+
+            dev_w = cuda.to_device(wi, stream=stream)
+            dev_r = cuda.to_device(ri, stream=stream)
+            dev_m = cuda.to_device(mutex, stream=stream)
+
+            stream_exchange[1, n_agents, stream](dev_w, dev_r, dev_m, wmin, mcs, rng_state)
+
+            dev_w.copy_to_host(wi, stream=stream)
+
+            event_end.record(stream=stream)
+        
+        events_beg.append(event_beg)
+        events_end.append(event_end)
+        # Ensure that the reference to the GPU arrays are deleted, this will
+        # ensure garbage collection at the exit of the context.
+        del dev_w, dev_r, dev_m
+
+    time.sleep(5)
+    for event_end in events_end:
+        event_end.synchronize()
+    
+    elapsed_times = [events_beg[0].elapsed_time(event_end) for event_end in events_end]
+    i_stream_last = np.argmax(elapsed_times)
+
+    print(f"Last stream: {i_stream_last}")
+    print(f"Total time {elapsed_times[i_stream_last]:.2f} ms")
